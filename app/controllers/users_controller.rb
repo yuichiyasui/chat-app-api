bcc13b46739a77f23d2444f0249d09dc27523c99
yuichiyasui <yassan.b.a.w@gmail.com>
2021-08-06 02:25:19 +0900

    uuidでユーザー情報を取得する処理を作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,9 @@ class UsersController < ApplicationController
         registered_user = User.create!(uuid: SecureRandom.uuid, name: params[:name])
         render json: { data: registered_user }
     end
+
+    def fetch
+        user = User.find_by(uuid: params[:user_id])
+        render json: { data: user }
+    end
 end
