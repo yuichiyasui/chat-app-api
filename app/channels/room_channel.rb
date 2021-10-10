21b10a0b7040c68b79998651b0f9f676b3122efc
yuichiyasui <yassan.b.a.w@gmail.com>
2021-10-10 23:42:17 +0900

    チャット機能を実装

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -1,16 +1,34 @@
 class RoomChannel < ApplicationCable::Channel
+
   def subscribed
     stream_from "room_#{params[:id]}"
-    puts "ルームID:#{params[:id]}が購読されました"
-    enter
+    logger.info "ルームID:#{params[:id]}の購読が開始されました"
+    enter(params[:uuid])
   end
 
-  def enter
+  def enter(uuid)
+    user = User.find_by(uuid: uuid)
+    res = { 
+      "type": "notification",
+      "message": "#{user.name}さんが入室しました", 
+      "createdAt": Time.now
+    }
+    ActionCable.server.broadcast "room_#{params[:id]}", res
+  end
+
+  def submit(data)
     user = User.find_by(uuid: params[:uuid])
-    ActionCable.server.broadcast "room_#{params[:id]}", "#{user.name}さんが入室しました"
+    res = { 
+      "type": "message",
+      "name": user.name,
+      "userId": user.id,
+      "message": data["message"],
+      "createdAt": Time.now
+    }
+    ActionCable.server.broadcast "room_#{params[:id]}", res
   end
 
   def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
+    logger.info "ルームID:#{params[:id]}の購読が終了されました"
   end
 end
