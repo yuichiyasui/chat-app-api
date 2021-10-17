756680c451a5f93e17eca2fa1ce21e2f37bcefd7
yuichiyasui <yassan.b.a.w@gmail.com>
2021-10-17 14:01:28 +0900

    ルーム情報の読み込み処理を追加

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,11 +1,16 @@
 class RoomsController < ApplicationController
+    def create
+        room = Room.new(name: params[:name])
+        room.save
+    end
+
     def show
         rooms = Room.all
         render json: { data: rooms }
     end
 
-    def create
-        room = Room.new(name: params[:name])
-        room.save
+    def fetch
+        room = Room.find(params[:id])
+        render json: { data: room }
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/rooms', to: 'rooms#show'
+  get '/rooms/:id', to: 'rooms#fetch'
   post '/rooms', to: 'rooms#create'
-  get '/users', to: 'users#fetch'
+  get '/users/:id', to: 'users#fetch'
   post '/users', to: 'users#register'
 end
